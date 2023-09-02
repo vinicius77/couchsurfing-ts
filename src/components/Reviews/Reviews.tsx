463834a34bce36a83ecf8cc3c122ce0c7bb8ed07
Vinicius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2023-09-02 09:57:31 +0300

    fix(review-component): small fix

--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -6,12 +6,7 @@ const Reviews = () => {
   const { name, loyaltyUser } = latestReviewer;
   const reviewsInfo = showReviewTotal(reviews.length, name, loyaltyUser);
 
-  return (
-    <>
-      <h5>{reviewsInfo}</h5>
-      <h3>Other Properties recommended to you:</h3>
-    </>
-  );
+  return <h5>{reviewsInfo}</h5>;
 };
 
 export default Reviews;
